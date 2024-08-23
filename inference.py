0ab0a5f94cd42db24bbc8cd66b3c7c94edc66d2b
anonymous <>
2024-08-23 18:33:47 +0800

    Add tokenizer.pad_token = tokenizer.eos_token

--- inference.py
+++ inference.py
@@ -25,7 +25,7 @@ def inference(args):
             revision=args.revision,
             debug=args.debug,
         )
-        tokenizer.pad_token = tokenizer.bos_token
+        tokenizer.pad_token = tokenizer.eos_token
 
     def query(code, code_context):
         lc_messages = complete_template.format_messages(
